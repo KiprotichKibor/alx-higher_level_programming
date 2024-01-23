1975ae4fb5bc962f58d1713a503085f9a3e0fefe
KiprotichKibor <kiborotich24@gmail.com>
2024-01-23 12:06:29 -0800

    calculating area of a square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,30 +1,31 @@
 #!/usr/bin/python3
+"""
+Defines a square.
+
+Attributes:
+- size (int): The size of the square.
+"""
 class Square:
     """
-    Defines a square.
+    Initializes a new square instance.
 
-    Attributes:
-    - size (int): The size of the square.
+    Parameters: 
+    - size (int, optional): The size of the square (default is 0).
     """
     def __init__(self, size=0):
-        """
-        Initializes a new square instance.
 
-        Parameters:
-        - size (int, optional): The size of the square (default is 0).
-        """
         if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >=0")
         else:
             self.__size = size
+    """
+    Calculates and returns the current square area.
 
+    Returns:
+    - int: The area of the square.
+    """
     def area(self):
-        """
-        Calculates and returns the current square area.
 
-        Returns:
-        - int: The area of the square.
-        """
         return self.__size ** 2
