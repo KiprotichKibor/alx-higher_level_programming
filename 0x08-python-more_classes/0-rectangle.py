4b9bd6ae0a1c00de59bcfdff573f6372e09dce82
KiprotichKibor <kiborotich24@gmail.com>
2024-01-29 09:55:27 -0800

    an empty class Rectangle that defines a rectangle

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 class Rectangle:
+    """
+    A class that defines a rectangle
+    An empty class with no objects
+    """
     pass
 if __name__ == '__main__':
     Rectangle.main()
