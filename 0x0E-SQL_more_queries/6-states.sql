52167c07e819c4f8b36dc0cd3d3fc06d236c7d14
KiprotichKibor <kiborotich24@gmail.com>
2024-03-14 15:17:54 -0700

    creating a database with a table with it

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,8 @@
+-- a script that creates the database hbtn_0d_usa
+-- also creates the table states (in the database hbtn_0d_usa)
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states(
+	id INT AUTO_INCREMENT,
+	name VARCHAR(256),
+	PRIMARY KEY(id)
+);
