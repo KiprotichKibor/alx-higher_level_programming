4acb663a19b40af03c6ba17e50278953892e3ea6
KiprotichKibor <kiborotich24@gmail.com>
2024-04-09 03:56:33 -0700

    a script that prints 3 lines using an array of string and a loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const strings = ["C is fun", "Python is cool", "JavaScript is amazing"];
+for (let i = 0; i < strings.length; i++){
+	const string = strings[i];
+	console.log(string);
+}
