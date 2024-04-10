4a35e0469241cb14ff17a4bd82005a00d90dd302
KiprotichKibor <kiborotich24@gmail.com>
2024-04-10 02:14:22 -0700

    a class Rectangle that defines a rectangle

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 class Rectangle {
-    constructor(w, h) {
-        //if (w <= 0 || h <= 0) {
-          //  throw new Error('Width and height must be positive numbers');
-        //}
-        this.width = w;
-        this.height = h;
-    }
+  constructor (w, h) {
+    // if (w <= 0 || h <= 0) {
+    //  throw new Error('Width and height must be positive numbers');
+    // }
+    this.width = w;
+    this.height = h;
+  }
 }
 
 module.exports = Rectangle;
