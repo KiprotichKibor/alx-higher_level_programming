207b5eb1c3ac32a4bf87caf4b6e1683e6e7807a0
KiprotichKibor <kiborotich24@gmail.com>
2024-04-15 03:03:12 -0700

    another test

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,9 +3,15 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
+    """
+    Check if the script is beigng run directly
+    """
     username = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
+    """
+    Retrieves username, password, and database name from command-line
+    """
 
     db = MySQLdb.connect(
         host="localhost",
@@ -14,12 +20,35 @@ if __name__ == "__main__":
         passwd=password,
         db=db_name
     )
+    """
+    connects to the MySQL server with the provided credentials
+    """
 
     cursor = db.cursor()
+    """
+    creates a cursor object to execute SQL queries
+    """
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    """
+    Executes MySQL query
+    select all rows from the table 'states'
+    sorted by the 'id' column in ascending order
+    """
     states = cursor.fetchall()
+    """
+    fetch all the results
+    """
     for state in states:
+        """
+        iterate through fetched results
+        and prints each row
+
+        Args:
+            state: the name of the state to be printed
+        """
         print(state)
 
     cursor.close()
+    """ closes the cursor object """
     db.close()
+    """ closes the database connection object """
