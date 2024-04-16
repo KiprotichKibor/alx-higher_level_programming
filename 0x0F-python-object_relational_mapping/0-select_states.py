618ed44cda3f296399167b76c7336253110cc8a4
KiprotichKibor <kiborotich24@gmail.com>
2024-04-16 01:59:02 -0700

    last time

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,53 +1,14 @@
 #!/usr/bin/python3
-import MySQLdb
+#Lists all states from the database hbtn_0e_0_usa.
+# Lists all states from the database hbtn_0e_0_usa.
+# Usage: ./0-select_states.py <mysql username> \
+#                             <mysql password> \
+#                             <database name>
 import sys
+import MySQLdb
 
-if __name__ == "__main__":
-    """
-    Check if the script is beigng run directly
-    """
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-    """
-    Retrieves username, password, and database name from command-line
-    """
-
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=db_name
-    )
-    """
-    connects to the MySQL server with the provided credentials """
-
-    cursor = db.cursor()
-    """
-    creates a cursor object to execute SQL queries
-    """
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
-    """
-    Executes MySQL query
-    select all rows from the table 'states'
-    sorted by the 'id' column in ascending order
-    """
-    states = cursor.fetchall()
-    """
-    fetch all the results
-    """
-    for state in states:
-        """
-        iterate through fetched results
-        and prints each row
-
-        Args:
-            state: the name of the state to be printed
-        """
-        print(state)
-
-    cursor.close()
-    """ closes the cursor object """
-    db.close()
-    """ closes the database connection object """
+if __name__ = '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `states`")
+    [print(state) for state in c.fetchall()]
