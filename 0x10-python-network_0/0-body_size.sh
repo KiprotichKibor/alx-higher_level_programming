23b653d8d9b90e97ade79b610954401ee036f659
KiprotichKibor <kiborotich24@gmail.com>
2024-04-26 03:10:58 -0700

    just trying

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a request to that URL
-response=$(curl -s "$1" -w "%{size_download}")
-echo "$response"
+response=$(curl -s "$1" -w "%{size_download}") ; echo "$response"
